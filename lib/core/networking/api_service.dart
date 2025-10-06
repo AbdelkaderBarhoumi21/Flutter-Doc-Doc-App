a1bb512f09de5709564e931ff3705328e7b56dde
Abdelkader Barhoumi <b.abdelkader23176@pi.tn>
2025-10-06 22:29:58 +0100

    feat(sigup):add signup screen UI and add signup API

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -2,6 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:docdoc_app/core/networking/api_constants.dart';
 import 'package:docdoc_app/features/login/data/models/login_request_body.dart';
 import 'package:docdoc_app/features/login/data/models/login_response.dart';
+import 'package:docdoc_app/features/sign_up/data/models/sign_up_request_body.dart';
+import 'package:docdoc_app/features/sign_up/data/models/sign_up_response.dart';
 import 'package:retrofit/retrofit.dart';
 part 'api_service.g.dart'; //make generate in this file => the name of file
 
@@ -11,4 +13,6 @@ abstract class ApiService {
 
   @POST(ApiConstants.login)
   Future<LoginResponse> login(@Body() LoginRequestBody loginRequestBody);
+  @POST(ApiConstants.signup)
+  Future<SignupResponse> signup(@Body() SignupRequestBody signupRequestBody);
 }
---
+++ lib/features/sign_up/data/repository/sign_up_repo.dart
@@ -0,0 +1,20 @@
+import 'package:docdoc_app/core/networking/api_error_handler.dart';
+import 'package:docdoc_app/core/networking/api_result.dart';
+import 'package:docdoc_app/core/networking/api_service.dart';
+import 'package:docdoc_app/features/sign_up/data/models/sign_up_request_body.dart';
+import 'package:docdoc_app/features/sign_up/data/models/sign_up_response.dart';
+
+class SignUpRepo {
+  final ApiService _apiService;
+  SignUpRepo(this._apiService);
+  Future<ApiResult<SignupResponse>> signUp(
+    SignupRequestBody signupRequestBody,
+  ) async {
+    try {
+      final response = await _apiService.signup(signupRequestBody);
+      return ApiResult.success(response);
+    } catch (error) {
+      return ApiResult.failure(ErrorHandler.handle(error));
+    }
+  }
+}
