2b2c0de10b062345cf99968d15f433069429fd80
Abdelkader Barhoumi <b.abdelkader23176@pi.tn>
2025-10-12 15:15:10 +0100

    -Enhanced doctors image shimmer loading

--- lib/features/home/ui/widgets/doctors/doctors_list_view_item.dart
+++ lib/features/home/ui/widgets/doctors/doctors_list_view_item.dart
@@ -1,8 +1,11 @@
 import 'package:docdoc_app/core/helpers/spacing.dart';
+import 'package:docdoc_app/core/themes/app_colors.dart';
 import 'package:docdoc_app/core/themes/app_styles.dart';
 import 'package:docdoc_app/features/home/data/models/specializations_response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class DoctorsListViewItem extends StatelessWidget {
   final Doctors? doctorsModel;
@@ -15,15 +18,43 @@ class DoctorsListViewItem extends StatelessWidget {
 
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12.0.sp),
-            child: Image.asset(
-              'assets/images/home_doctor.png',
+          CachedNetworkImage(
+            imageUrl:
+                "https://static.wikia.nocookie.net/five-world-war/images/6/64/Hisoka.jpg/revision/latest?cb=20190313114050",
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Shimmer.fromColors(
+                baseColor: AppColors.lightGrey,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            imageBuilder: (context, imageProvider) => Container(
               width: 110.w,
-              height: 110.h,
-              fit: BoxFit.contain,
+              height: 120.h,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+              ),
             ),
           ),
+          // ClipRRect(
+          //   borderRadius: BorderRadius.circular(12.0.sp),
+          //   child: Image.asset(
+          //     'assets/images/home_doctor.png',
+          //     width: 110.w,
+          //     height: 110.h,
+          //     fit: BoxFit.contain,
+          //   ),
+          // ),
           horizontalSpace(16),
           Expanded(
             child: Column(
